38d66337d9357f294ba97e4f2b0a3b57d11cd05a
Dianyela Maldonado <dianyela.y.maldonado@gmail.com>
2020-09-05 12:32:10 -0500

    I added navbar user component

---
+++ src/components/navbarUser/NavbarUser.js
@@ -0,0 +1,295 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import HomeIcon from '@material-ui/icons/Home';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import LocalAtmSharpIcon from '@material-ui/icons/LocalAtmSharp';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
+
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//         width: '100%',
+//     },
+//     heading: {
+//         fontSize: theme.typography.pxToRem(15),
+//         flexBasis: '33.33%',
+//         flexShrink: 0,
+//     },
+//     secondaryHeading: {
+//         fontSize: theme.typography.pxToRem(15),
+//         color: theme.palette.text.secondary,
+//     },
+// }));
+
+export default function SearchAppBar() {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef(null);
+
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    };
+
+
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    }
+
+    function handleListKeyDown(event) {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            setOpen(false);
+        }
+    }
+
+    // return focus to the button when we transitioned from !open -> open
+    const prevOpen = React.useRef(open);
+    React.useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+            anchorRef.current.focus();
+        }
+
+        prevOpen.current = open;
+    }, [open]);
+
+    // const prevOpen = React.useRef(open);
+    // const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="open drawer"
+                        ref={anchorRef}
+                        aria-controls={open ? 'menu-list-grow' : undefined}
+                        aria-haspopup="true"
+                        onClick={handleToggle}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+
+                    <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+                        {({ TransitionProps, placement }) => (
+                            <Grow
+                                {...TransitionProps}
+                                style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+                            >
+                                <Paper>
+                                    {/* <img src="assets/nextia-icon.png" alt="" /> */}
+                                    <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                                        <MenuItem onClick={handleClose}>
+
+                                            <ListItemIcon>
+                                                <HomeIcon fontSize="small" />
+                                            </ListItemIcon>
+
+                                            <Typography variant="inherit">Inicio</Typography>
+                                        </MenuItem>
+                                    </MenuList>
+                                    <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                                        <AccordionSummary
+                                            expandIcon={<ExpandMoreIcon />}
+                                            aria-controls="panel1bh-content"
+                                            id="panel1bh-header"
+                                        >
+                                            <Typography className={classes.heading}>Productos</Typography>
+                                        </AccordionSummary>
+                                        <AccordionDetails>
+                                            <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                                                <MenuItem onClick={handleClose}>Cacao</MenuItem>
+                                                <MenuItem onClick={handleClose}>Café</MenuItem>
+                                                <MenuItem onClick={handleClose}>Madera</MenuItem>
+                                                <MenuItem onClick={handleClose}>Miel</MenuItem>
+                                                <MenuItem onClick={handleClose}>Otros productos</MenuItem>
+                                            </MenuList>
+                                        </AccordionDetails>
+                                    </Accordion>
+
+                                    <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                                        <AccordionSummary
+                                            expandIcon={<ExpandMoreIcon />}
+                                            aria-controls="panel2bh-content"
+                                            id="panel2bh-header"
+                                        >
+                                            <Typography className={classes.heading}>Artesanías</Typography>
+                                        </AccordionSummary>
+                                        <AccordionDetails>
+                                            <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                                                <MenuItem onClick={handleClose}>Barro</MenuItem>
+                                                <MenuItem onClick={handleClose}>Fibras vegetales</MenuItem>
+                                                <MenuItem onClick={handleClose}>Metales</MenuItem>
+                                                <MenuItem onClick={handleClose}>Textiles</MenuItem>
+                                                <MenuItem onClick={handleClose}>Varios</MenuItem>
+                                            </MenuList>
+                                        </AccordionDetails>
+                                    </Accordion>
+
+                                    <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                                        <AccordionSummary
+                                            expandIcon={<ExpandMoreIcon />}
+                                            aria-controls="panel3bh-content"
+                                            id="panel3bh-header"
+                                        >
+                                            <Typography className={classes.heading}>Zona del país</Typography>
+                                        </AccordionSummary>
+                                        <AccordionDetails>
+                                            <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                                                <MenuItem onClick={handleClose}>Norte</MenuItem>
+                                                <MenuItem onClick={handleClose}>Sur</MenuItem>
+                                                <MenuItem onClick={handleClose}>Centro</MenuItem>
+                                            </MenuList>
+                                        </AccordionDetails>
+                                    </Accordion>
+
+                                    <MenuList autoFocusItem={open} id="menu-list-grow-2" onKeyDown={handleListKeyDown}>
+
+                                        <MenuItem onClick={handleClose}>
+                                            <Typography variant="inherit">Blog</Typography>
+                                        </MenuItem>
+
+                                        <MenuItem onClick={handleClose}>
+                                            <Typography variant="inherit">Ayuda</Typography>
+                                        </MenuItem>
+
+                                        <MenuItem onClick={handleClose}>
+                                            <ListItemIcon>
+                                                <AccountCircleIcon fontSize="small" />
+                                            </ListItemIcon>
+                                            <Typography variant="inherit">Mi perfil</Typography>
+                                        </MenuItem>
+
+                                        <MenuItem onClick={handleClose}>
+                                            <ListItemIcon>
+                                                <LocalAtmSharpIcon fontSize="small" />
+                                            </ListItemIcon>
+                                            <Typography variant="inherit">Mis pedidos</Typography>
+                                        </MenuItem>
+
+                                        <MenuItem onClick={handleClose}>
+                                            <ListItemIcon>
+                                                <FavoriteIcon fontSize="small" />
+                                            </ListItemIcon>
+                                            <Typography variant="inherit">Favoritos</Typography>
+                                        </MenuItem>
+
+                                        <MenuItem onClick={handleClose}>
+                                            <ListItemIcon>
+                                                <PowerSettingsNewIcon fontSize="small" />
+                                            </ListItemIcon>
+                                            <Typography variant="inherit">Cerrar Sesión</Typography>
+                                        </MenuItem>
+                                    </MenuList>
+
+                                </Paper>
+                            </Grow>
+                        )}
+                    </Popper>
+
+                </Toolbar>
+
+            </AppBar>
+
+            <div className={classes.search}>
+
+
+
+            </div>
+        </div>
+    );
+}
+
+
+
+
