9194885c690481a54d0d21e2775964c75ce1c333
gaby <gabyalvarzb@gmail.com>
2020-09-09 21:03:00 -0500

    delete coments

--- src/components/payment/PaymentPayPal.js
+++ src/components/payment/PaymentPayPal.js
@@ -17,12 +17,12 @@ const PaymentPayPal = () => {
             // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
                 onSuccess={(details, data) => {
                     alert("Detail" + JSON.stringify(details));
-                    alert("Data" + JSON.stringify(data));
+                    /* alert("Data" + JSON.stringify(data)); */
                     
-                    let { create_time, id, payer,status } = details
+                    let {create_time, id, payer,status} = details
                     
                     /* setTimeout(() => diAlgo("pasaron 10 segundos"), 10000); */
-                    return saveTransaction({create_time,payer,id,status})                     
+                    return saveTransaction({create_time,payer,id,status})                    
                 }}
                 catchError = { (err) => {
                     alert("No se pudo procesar" + JSON.stringify(err));
